9c78b787fcd50b7150885ae997ad89de7b129d8f
wuqiiw <polwuqiiw@gmail.com>
2025-11-20 11:18:18 +0300

    Delete rust-labs/lab_1/lab_2 directory

--- rust-labs/lab_1/lab_2/target/debug/deps/greeting-c746d5ce6c385184.d
+++
@@ -1,5 +0,0 @@
-/Users/wuqiiw/Documents/Программирование/Programming-C-labs/rust-labs/lab_1/lab_2/target/debug/deps/greeting-c746d5ce6c385184.d: src/main.rs
-
-/Users/wuqiiw/Documents/Программирование/Programming-C-labs/rust-labs/lab_1/lab_2/target/debug/deps/greeting-c746d5ce6c385184: src/main.rs
-
-src/main.rs:
